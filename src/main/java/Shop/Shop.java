05e155bc1b2089455d8ef25c39a79926c3d5d030
Fraser McKay <frasermckay97@gmail.com>
2018-05-13 16:00:46 +0100

    creqted shop

---
+++ src/main/java/Shop/Shop.java
@@ -0,0 +1,28 @@
+package Shop;
+
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
+  public class Shop {
+
+     private ArrayList<ISell> stock;
+
+     public Shop() {
+         this.stock = new ArrayList<>();
+    }
+
+     public int stockCount() {
+        return stock.size();
+    }
+
+    public void addStock(ISell item) {
+          this.stock.add(item);
+      }
+
+
+      public void removeStock(ISell item) {
+          this.stock.remove(item);
+      }
+
+}
